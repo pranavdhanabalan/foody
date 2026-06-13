d8a4998ade632f05ea8285cce540377bae80c1fb
pranavdhanabalan <dhanabalanpranav@gmail.com>
2026-06-14 03:30:17 +0530

    Merge remote-tracking branch 'origin/master'

--- src/main/java/com/pranavd/foody/Model/Order.java
+++ src/main/java/com/pranavd/foody/Model/Order.java
@@ -1,4 +1,29 @@
 package com.pranavd.foody.Model;
 
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long orderId;
+
+    @Column(nullable = false)
+    private LocalDate orderDate;
+
+    @Column(nullable = false)
+    private LocalTime orderTime;
+
+    private String orderStatus;
+
+    private BigDecimal totalPrice;
+
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
+    private List<OrderItem> orderItems;
 }
--- src/main/java/com/pranavd/foody/Model/OrderItem.java
+++ src/main/java/com/pranavd/foody/Model/OrderItem.java
@@ -1,4 +1,41 @@
 package com.pranavd.foody.Model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderItemId;
+
+    private int quantity;
+
+    @ManyToOne
+    @JoinColumn(name="order_id")
+    private Order order;
+
+    public Long getOrderItemId() {
+        return orderItemId;
+    }
+
+    public void setOrderItemId(Long orderItemId) {
+        this.orderItemId = orderItemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
